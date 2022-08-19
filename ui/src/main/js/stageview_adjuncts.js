45f426fef42322894453f3c735b8b4eea277228f
Thorsten Scherler <scherler@gmail.com>
2022-08-19 13:49:40 +0200

    chore: [feature/feat/parcel-extp-jsmodules] fix for adjuncts
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

---
+++ ui/src/main/js/stageview_adjuncts.js
@@ -0,0 +1,5 @@
+var jsModules = require('@jenkins-cd/js-modules');
+jsModules.addScript(
+    'plugin/pipeline-stage-view/jsmodules/stageview.js',
+    'jenkins-js-module:pipeline-stage-view:stageview:js'
+);
